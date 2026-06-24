e2c5c515b9b25bbada6110e3fc89b6ab25e9eac8
mariaiza-commits <mariaizaramos6@gmail.com>
2026-06-24 17:54:33 -0300

    fix: desregistrar service worker antigo automaticamente

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+// Remove qualquer service worker antigo em cache
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(reg => reg.unregister())
+  })
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<React.StrictMode><App /></React.StrictMode>)
